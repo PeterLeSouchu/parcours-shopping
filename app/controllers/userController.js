e14d6c757eabe64d695c9d77ca5ba6c774a95eb4
PeterLeSouchu <peter22510@gmail.com>
2024-04-16 10:56:36 +0200

    methode register terminée

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -9,16 +9,46 @@ const userController = {
 
     register: async (req, res) => {
         try {
+            const { name, email, password, passwordConfirm } = req.body;
             // !! votre code à partir d'ici
-            // verifier l'email avec le package npm email-validator
+            // On vérifie l'email avec le package npm email-validator
+            emailValidator.validate(email);
 
-            // verifier si password correspond à password confirm
+            // On vérifie si les mdp correspondent
+            if (password !== passwordConfirm) {
+                res.status(400).send('Les mots de passe ne correspondent pas');
+                return;
+            }
+            // On vérifie si l'utilisateur existe en BDD
+            const userWithSameEmail = await User.findOne({
+                where: {
+                    email: email,
+                },
+            });
 
-            // hash password
+            if (userWithSameEmail) {
+                // Si il existe déjà un utilisateur avec cet email, on l'envoie sur la page de connexion
+                res.redirect('/login');
+                return;
+            }
 
-            // attribuer un rôle ici, le role customer.
+            // On hash le mdp avec un nombre de tour de hachage de 10
+            const passwordHashed = await bcrypt.hash(password, 10);
 
-            // sauvegarder user
+            // On va chercher en bdd le role customer que l'on stocke dans une varaible.
+            const role = await Role.findOne({
+                where: {
+                    name: 'customer',
+                },
+            });
+
+            // On créer  l'utilisateur avec le role customer en l'inserant en BDD
+            await User.create({
+                name,
+                email,
+                password: passwordHashed,
+                role_id: role.id,
+            });
 
             // !! ne pas modifier cette ligne
             res.render('login', {
