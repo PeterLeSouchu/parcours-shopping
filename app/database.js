12bb2ccff12589e007998d26e10f5da2667a9254
PeterLeSouchu <peter22510@gmail.com>
2024-08-05 16:23:45 +0200

    feat: start add ssl in client sequelize

--- app/database.js
+++ app/database.js
@@ -1,6 +1,12 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.PG_URL, {
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // Utilisé pour bypasser le certificat non sécurisé, si nécessaire
+        },
+    },
     // logging false pour ne pas polluer le terminal avec les requêtes
     // si on veut voir les requêtes, il faut enlever cette ligne
     // logging: false,
